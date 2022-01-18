38007c7ebcf387e026d303711d60bd44b73acfaf
lamnguyenptit <lamnguyenptit99@gmail.com>
2022-01-18 09:25:27 +0700

    lam

--- src/main/java/com/example/login/config/WebSecurityConfig.java
+++ src/main/java/com/example/login/config/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig{
                         .loginPage("/loginAdmin")
                         .loginProcessingUrl("/admin_login")
                         .failureUrl("/loginAdmin?error")
-                        .defaultSuccessUrl("/admin/home")
+                        .defaultSuccessUrl("/admin/home", true)
                         .permitAll()
 
                         .and()
@@ -112,7 +112,7 @@ public class WebSecurityConfig{
                         .loginPage("/loginUser")
                         .loginProcessingUrl("/user_login")
                         .failureUrl("/loginUser?error")
-                        .defaultSuccessUrl("/user/home")
+                        .defaultSuccessUrl("/user/home", true)
                         .permitAll()
 
                         .and()
